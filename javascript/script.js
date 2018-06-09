a8852dd6077eef828ddf7405cf3d25bfd38cdaac
Aaron Suman <oneajs@gmail.com>
2018-06-09 16:05:13 +0100

    V1.0.5 Added sticky secondary navbar + hamburger icon
    New animated hamburger icon for when in mobile mode.
    Made the secondary navbar sticky on the experience, education and projects pages for easier navigation around those individual pages. Could make main navbar sticky too but don't think it's necessary at the moment.

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,3 @@
-//animated slide code from Valentin Sarychev
-$(document).on('click', 'a[href^="#"]', function(e) {
-  var id = $(this).attr('href')
-  var $id = $(id)
-  if ($id.length === 0) return
-  e.preventDefault()
-  var position = $id.offset().top
-  $('body, html').animate({scrollTop: position})
-});
-
 //fade in and out back to top button
 $(document).on("scroll", function () {
   var btn = document.getElementById('totop');
@@ -16,4 +6,29 @@ $(document).on("scroll", function () {
   } else {
     $(btn).removeClass("btnvis")
   }
+})
+
+// change hamburger icon on click - need to make more efficient
+$(document).on('click', 'button[id="navicon"]', function() {
+  var icon = document.getElementById('navicon');
+  icon.classList.toggle("is-active")
+})
+
+// stick secondary navbar to top of pageYOffset
+window.onscroll = function() {stick()}
+var sub = document.getElementById("subnavbar")
+function stick() {
+  if (window.pageYOffset >= sub.offsetTop) {
+    sub.classList.add("sticky-top")
+  } else sub.classList.remove("sticky-top")
+}
+
+//animated slide code from Valentin Sarychev
+$(document).on('click', 'a[href^="#"]', function(e) {
+  var id = $(this).attr('href')
+  var $id = $(id)
+  if ($id.length === 0) return
+  e.preventDefault()
+  var position = $id.offset().top
+  $('body, html').animate({scrollTop: position})
 })
