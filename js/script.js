6ac5d36d0705f8ecf0b9a1d0540630824bee12e4
Aaron Suman <oneajs@gmail.com>
2021-02-10 11:59:46 +0000

    Update to version 2.0.0

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
-//fade in and out back to top button
-$(document).on("scroll", function () {
-  var btn = document.getElementById('totop');
-  if (window.pageYOffset > 500) $(btn).addClass("btnvis")
-  else $(btn).removeClass("btnvis")
-})
-
 // change hamburger icon on click - need to make more efficient
 $(document).on('click', 'button[id="navicon"]', function() {
   var icon = document.getElementById('navicon');
   icon.classList.toggle("is-active")
 })
 
-//animated slide code. Original from Valentin Sarychev, I made some slight modifications to accommodate my needs.
-// $(document).on('click', 'a[href^="#"]', function(e) {
-//   var id = $(this).attr('href')
-//   var $id = $(id)
-//   if ($id.length === 0) return
-//   e.preventDefault()
-//   var position = $id.offset().top - 40
-//   if (id === '#studentforum2018' || id === '#projectmanager2018') position -= 60
-//   if (!id.includes('year')) $('body, html').animate({scrollTop: position})
-// })
+$(document).on('click', 'a[href^="#"]', function(e) {
+  var id = $(this).attr('href')
+  var $id = $(id)
+  if ($id.length === 0) return
+  e.preventDefault()
+  var position = $id.offset().top
+  $('body, html').animate({scrollTop: position})
+});
+
+$('body').scrollspy({ target: '#resumebar' })
+
+const btn = document.querySelector(".darkmodeToggle");
+const currentTheme = localStorage.getItem("theme");
+const main = document.getElementById("main");
+
+if (currentTheme == "dark") {
+  main.classList.remove("bg-white");
+  main.classList.add("bg-dark");
+  main.classList.add("text-darkmode");
+}
+
+btn.addEventListener("click", function () {
+  main.classList.toggle("bg-white");
+  main.classList.toggle("bg-dark");
+  main.classList.toggle("text-darkmode");
+  let theme = "light";
+  if (main.classList.contains("bg-dark")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
